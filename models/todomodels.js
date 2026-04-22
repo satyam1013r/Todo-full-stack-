d7537a8af1c19a62e2a24c626b129be13e3b87bf
satyam1013r <satyamshivam098890@gmail.com>
2026-04-22 19:06:34 +0530

    create todo model

---
+++ models/todomodels.js
@@ -0,0 +1,15 @@
+const mongoose=require("mongoose")
+const TodoSchema=new mongoose.Schema({
+    task:{
+        type:String,
+        required:true
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId(),// see as id 
+        ref:"User",                         
+                                                     // tells which model it connects to? model name write  it tell mongoose This userId comes from the User model”
+        required:true
+    }
+},{timestamps:true})// add createAt and updateAt?
+
+module.exports=mongoose.model("Todo",TodoSchema)
