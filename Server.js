28e6fec518d4daa2100468a91c6932c74354f503
satyam1013r <satyamshivam098890@gmail.com>
2026-04-23 17:42:22 +0530

    setup express with routes and database connection

---
+++ Server.js
@@ -0,0 +1,35 @@
+require("dotenv").config()
+const express=require("express")
+const cors=require("cors")
+const todoRoutes=require("./Routes/todoRoutes.js")
+const UserRoutes=require("./Routes/userRoutes.js")
+const DataBase=require("./config/config.js")
+const path=require("path")
+
+
+
+const app=express()
+//connect middleware
+app.use(express.json())
+app.use(cors())
+app.use(express.static(path.join(__dirname,"public")))
+
+//database connect
+DataBase()
+
+// routes set
+app.get("/",(req,res)=>{
+    res.send("Welcome to my TODO")
+})
+
+app.use("/api",todoRoutes)
+app.use("/api",UserRoutes)
+
+//port 
+const port=process.env.PORT||3000
+
+
+// listen 
+app.listen(port,()=>{
+    console.log(`App listening on port ${port}`)
+})
